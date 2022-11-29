ffe3de4299f978f4f821cbe4a24ccfc334462cf6
utkkhare299 <utkkhare999@gmail.com>
2022-11-29 14:27:38 +0530

    first code9

---
+++ demo.js
@@ -0,0 +1,9 @@
+console.log("I love git")
+console.log("I love git")
+
+const headerTitle = document.getElementById('header-title')
+const title1 = document.getElementsByClassName('title')
+
+headerTitle.style.borderBottom = 'solid 3px black'
+title1[0].style.fontWeight = 'bold'
+title1[0].style.color = 'green'
